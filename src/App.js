40d8ba57ceac376e8e453e0839fc2f96a9a7d40b
itsmohiful <mohiful.islam.ofc@gmail.com>
2023-05-29 01:22:26 +0600

    home page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import IndexComponent from "./pages";
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <IndexComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "../components/Home.component"
+import 'bootstrap/dist/css/bootstrap.css';
+export default function IndexComponent(){
+    return (
+        <>
+            <Home />
+        </>
+    )
+}
